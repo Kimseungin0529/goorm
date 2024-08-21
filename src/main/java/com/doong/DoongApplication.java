125e28c2d5d6311fb32e92162726d37008e02b7b
Kimseungin0529 <whffkaos007@naver.com>
2024-08-21 11:56:48 +0900

    구름 test commit

--- src/main/java/com/doong/DoongApplication.java
+++ src/main/java/com/doong/DoongApplication.java
@@ -3,11 +3,21 @@ package com.doong;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class DoongApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoongApplication.class, args);
+		Scanner sc = new Scanner(System.in);
+		int age = 25;
+		String name = "김승진";
+		introduceSelf(age, name);
+
+		//SpringApplication.run(DoongApplication.class, args);
+	}
+	public static void introduceSelf(int myAge, String myName){
+		System.out.println("저의 나이는 " + myAge + "이고 이름은 " + myName + "입니다.");
 	}
 
 }
