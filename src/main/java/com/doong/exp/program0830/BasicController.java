5384b1b242009bfe8de0d01f0186a352736da494
Kimseungin0529 <whffkaos007@naver.com>
2024-09-03 14:41:14 +0900

    EXP 개인 미션 - 0830

---
+++ src/main/java/com/doong/exp/program0830/BasicController.java
@@ -0,0 +1,13 @@
+package com.doong.exp.program0830;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @GetMapping("/")
+    public String showBasicString(){
+        return "Hello world!";
+    }
+}
